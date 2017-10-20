1be5af2d87bf41ee2d9dca6bf89784f5b09bde88
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-10-20 08:59:48 +0100

    Sketch of automatic argument generation
    This does not actually work but should contain most of the important
    bits.  There's a big question of the best way of getting the client
    to interact here

---
+++ R/swagger_args.R
@@ -0,0 +1,173 @@
+## Autoatic argument handling; read through the swagger spec and
+## create a *default* argument handler.  This is going to require
+## quite a bit of case-by-case work for nontrivial things.  There's
+## also lots of translation required so that we can keep the primarily
+## snake-case interface to the package working.
+
+foo <- function(method, path, spec) {
+  ## All the stopifnot bits are assertions that have more to do with
+  ## making sure that the spec confirms to what we are expecting.
+  ## They'd probably be better done with debugme because I don't think
+  ## they should be run by users.
+  x <- spec$paths[[path]][[method]]
+
+  pars <- x$parameters
+  pars_in <- vcapply(pars, "[[", "in")
+  pars_optional <- !vlapply(pars, function(x)
+    isTRUE(x$required) || x[["in"]] == "body")
+
+  i <- match(pars_in, c("path", "body", "query", "header"))
+  stopifnot(all(!is.na(i)))
+  ord <- order(pars_optional, i)
+
+  if (!identical(ord, seq_along(ord))) {
+    pars_in <- pars_in[ord]
+    pars_optional <- pars_optional[ord]
+    pars <- pars[ord]
+  }
+
+  pars_name <- vcapply(pars, "[[", "name")
+  pars_name_r <- pars_name
+  pars_name_r[pars_in == "header"] <-
+    name_header_to_r(pars_name[pars_in == "header"])
+
+  if (any(duplicated(pars_name))) {
+    stop("fix duplicated names")
+  }
+  stopifnot(identical(pars_name[pars_in == "path"], parse_path(path)$args))
+  stopifnot(sum(pars_in == "body") <= 1L)
+
+  ## These bits here can likely get completely overhauled as there's
+  ## heaps of duplication!
+  dest <- quote(dest)
+  fbody_path <- lapply(pars[pars_in == "path"], arg_collect_path)
+  if (length(fbody_path) > 0L) {
+    fbody_path <- as.call(c(list(quote(c)), fbody_path))
+    fbody_path <- bquote(.(dest)$path <- .(fbody_path))
+  }
+  fbody_query <- lapply(pars[pars_in == "query"], arg_collect_query, dest)
+  if (length(fbody_query) > 0L) {
+    fbody_query <- c(bquote(.(dest)$query <- list()), fbody_query)
+  }
+  if (any(pars_in == "body")) {
+    fbody_body <- arg_collect_body(pars[[which(pars_in == "body")]], dest)
+    fbody_body <- bquote(.(dest)$body <- .(fbody_body))
+  } else {
+    fbody_body <- NULL
+  }
+  fbody_header <- lapply(pars[pars_in == "header"], arg_collect_header, dest)
+  if (length(fbody_header) > 0L) {
+    fbody_header <- c(bquote(.(dest)$header <- list()), fbody_header)
+  }
+
+  fbody <- as.call(c(quote(`{`), bquote(.(dest) <- list()),
+                     fbody_path, fbody_query, fbody_body, fbody_header,
+                     dest))
+
+  a <- rep(alist(. =, . = NULL), c(sum(!pars_optional), sum(pars_optional)))
+  names(a) <- pars_name_r
+  env <- parent.env(environment())
+  as.function(c(a, fbody), env)
+}
+
+arg_collector <- function(p, dest) {
+  switch(p[["in"]],
+         path = arg_collect_path,
+         query = arg_collect_query,
+         body = arg_collect_body,
+         header = arg_collect_header,
+         stop("assertion error"))(p, dest)
+}
+
+arg_collect_path <- function(p, dest) {
+  if (!isTRUE(p$required)) {
+    stop("all path parameters assumed required")
+  }
+  validate <- quote(assert_scalar_character)
+  bquote(.(validate)(.(as.symbol(p$name))))
+}
+
+arg_collect_query <- function(p, dest) {
+  type <- p$type
+  if (type == "boolean") {
+    validate <- quote(as_query_logical)
+  } else if (type == "integer") {
+    validate <- quote(assert_scalar_integer)
+  } else if (type == "string") {
+    if (grepl("json", p$description, ignore.case = TRUE)) {
+      validate <- quote(as_query_json)
+    } else {
+      validate <- quote(assert_scalar_character)
+    }
+  } else if (type == "array") {
+    if (identical(p$items, list(type = "string"))) {
+      validate <- quote(as_query_array_string)
+    } else {
+      stop("Can't handle this sort of array")
+    }
+  } else {
+    stop("Can't handle this sort of thing")
+  }
+  nm <- as.symbol(p$name)
+  expr <- bquote(.(dest)$query[[.(p$name)]] <- .(validate)(.(nm)))
+  if (!isTRUE(p$required)) {
+    expr <- bquote(if (!is.null(.(nm))) .(expr))
+  }
+  expr
+}
+
+arg_collect_body <- function(p, dest) {
+  ## These ones here pretty much will require a bunch of work; we can
+  ## probably outsource a lot of the common bits but all bodies are
+  ## nontrivial
+  ## POST /containers/create (json data)
+  ## POST /conatainers/{id}/update (json data)
+  ## PUT /containers/{id}/update (tar string)
+  ## POST /build (tar string)
+  ## POST /images/create (tar string)
+  ## POST /auth (json data)
+  ## POST /commit (json data)
+  ## POST /images/load (tar string)
+  ## POST /containers/{id}/exec (json data)
+  ##
+  ## For now I can just arrange to pause when I get the data
+  expr <- quote(browser())
+}
+
+arg_collect_header <- function(p, dest) {
+  stopifnot(p$type == "string")
+  r_nm <- as.symbol(name_header_to_r(p$name))
+  if (is.null(p$enum)) {
+    expr <- bquote(assert_scalar_character(.(r_nm)))
+  } else {
+    values <- as.call(c(quote(c), p$enum))
+    expr <- bquote(match_value(.(r_nm), .(values)))
+  }
+  expr <- bquote(.(dest)$header[[.(p$name)]] <- .(expr))
+  if (!isTRUE(p$required)) {
+    expr <- bquote(if (!is.null(.(r_nm))) .(expr))
+  }
+  expr
+}
+
+as_query_logical <- function(x, name = deparse(substitute(x))) {
+  assert_scalar_logical(x, name)
+  if (x) "true" else "false"
+}
+
+as_query_json <- function(x, name = deparse(substitute(x))) {
+  browser()
+}
+
+as_query_array_string <- function(x, name = deparse(substitute(x))) {
+  assert_character(x, name)
+  paste(x, collapse = ",")
+}
+
+as_body <- function(x, name = deparse(substitute(x))) {
+  browser()
+}
+
+name_header_to_r <- function(x) {
+  sub("^x_", "", gsub("-", "_", tolower(x)))
+}
---
+++ R/util_assert.R
@@ -0,0 +1,84 @@
+assert_scalar_character <- function(x, name = deparse(substitute(x))) {
+  assert_scalar(x, name)
+  assert_character(x, name)
+  assert_nonmissing(x, name)
+  invisible(x)
+}
+
+assert_scalar_integer <- function(x, strict = FALSE,
+                                  name = deparse(substitute(x))) {
+  assert_scalar(x, name)
+  assert_nonmissing(x, name)
+  assert_integer(x, strict, name)
+  invisible(x)
+}
+
+assert_scalar_logical <- function(x, name = deparse(substitute(x))) {
+  assert_scalar(x, name)
+  assert_nonmissing(x, name)
+  assert_logical(x, name)
+  invisible(x)
+}
+
+assert_scalar <- function(x, name = deparse(substitute(x))) {
+  if (length(x) != 1) {
+    stop(sprintf("'%s' must be a scalar", name), call. = FALSE)
+  }
+  invisible(x)
+}
+assert_nonmissing <- function(x, name = deparse(substitute(x))) {
+  if (any(is.na(x))) {
+    stop(sprintf("'%s' must not be NA", name), call. = FALSE)
+  }
+  invisible(x)
+}
+
+assert_character <- function(x, name = deparse(substitute(x))) {
+  if (!is.character(x)) {
+    stop(sprintf("'%s' must be a character", name), call. = FALSE)
+  }
+  invisible(x)
+}
+
+assert_raw <- function(x, name = deparse(substitute(x))) {
+  if (!is.raw(x)) {
+    stop(sprintf("'%s' must be raw", name), call. = FALSE)
+  }
+  invisible(x)
+}
+
+assert_logical <- function(x, name = deparse(substitute(x))) {
+  if (!is.logical(x)) {
+    stop(sprintf("'%s' must be logical", name), call. = FALSE)
+  }
+  invisible(x)
+}
+
+assert_is <- function(x, what, name = deparse(substitute(x))) {
+  if (!inherits(x, what)) {
+    stop(sprintf("'%s' must be a %s", name,
+                 paste(what, collapse = " / ")), call. = FALSE)
+  }
+  invisible(x)
+}
+
+assert_integer <- function(x, strict = FALSE, name = deparse(substitute(x))) {
+  if (!(is.integer(x))) {
+    usable_as_integer <-
+      !strict && is.numeric(x) && (max(abs(as.integer(x) - x)) < 1e-8)
+    if (!usable_as_integer) {
+      stop(sprintf("%s must be integer", name), call. = FALSE)
+    }
+  }
+  invisible(x)
+}
+
+match_value <- function(x, values, name = deparse(substitute(x))) {
+  assert_scalar_character(x, name)
+  if (is.na(match(x, values))) {
+    stop(sprintf("'%s' must be one of %s",
+                 name, paste(sprintf("'%s'", values), collapse = ", ")),
+         call. = FALSE)
+  }
+  x
+}
