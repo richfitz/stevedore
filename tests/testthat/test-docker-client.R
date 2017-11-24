a529fee4ae7c3f9cada5665607d8ddbcc32c9f07
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-11-24 08:15:02 +0000

    Testing for the main object

--- tests/testthat/test-docker-client.R
+++ tests/testthat/test-docker-client.R
@@ -1,12 +1,51 @@
 context("docker client")
 
 ## The most simple nontrivial thing with the docker client
-test_that("construction", {
-  docker <- docker_client(api_version)
-  expect_is(docker, "stevedore_object")
-  expect_is(docker, "docker_client")
-  ok <- docker$ping()
+test_that("ping", {
+  d <- docker_client()
+  expect_is(d, "stevedore_object")
+  expect_is(d, "docker_client")
+  ok <- d$ping()
   expect_equivalent(ok, "OK")
   expect_equal(names(attributes(ok)),
                c("api_version", "docker_experimental"))
+})
+
+test_that("info", {
+  d <- docker_client()
+  info <- d$info()
+  expect_is(info, "list")
+})
+
+test_that("version", {
+  d <- docker_client()
+  v <- d$version()
+  expect_is(v, "list")
+  expect_is(v$api_version, "character")
+})
+
+test_that("df", {
+  skip("df containers output looks broken to me")
+  d <- docker_client()
+  df <- d$df()
+})
+
+test_that("events", {
+  skip("events this needs some streaming support")
+  d <- docker_client()
+})
+
+test_that("children", {
+  d <- docker_client()
+  expect_is(d$containers, "docker_container_collection")
+  expect_is(d$containers, "stevedore_object")
+
+  expect_is(d$images, "docker_image_collection")
+  expect_is(d$images, "stevedore_object")
+
+  expect_is(d$networks, "docker_network_collection")
+  expect_is(d$networks, "stevedore_object")
+
+  expect_is(d$volumes, "docker_volume_collection")
+  expect_is(d$volumes, "stevedore_object")
 })
