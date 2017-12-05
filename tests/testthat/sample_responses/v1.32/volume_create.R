14f7a3aa95cf646b1e0c511a3fc98e96860c3edb
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-12-05 17:45:37 +0000

    Response tests for 1.32

---
+++ tests/testthat/sample_responses/v1.32/volume_create.R
@@ -0,0 +1,16 @@
+## version: 1.32
+## method: post
+## path: /volumes/create
+## code: 201
+## response: {"Name":"tardis","Driver":"custom","Mountpoint":"/var/lib/docker/volumes/tardis","Status":{"hello":"world"},"Labels":{"com.example.some-label":"some-value","com.example.some-other-label":"some-other-value"},"Scope":"local","CreatedAt":"2016-06-07T20:31:11.853781916Z"}
+list(name = "tardis",
+     driver = "custom",
+     mountpoint = "/var/lib/docker/volumes/tardis",
+     created_at = "2016-06-07T20:31:11.853781916Z",
+     status = list(hello = "world"),
+     labels = c(
+       "com.example.some-label" = "some-value",
+       "com.example.some-other-label" = "some-other-value"),
+     scope = "local",
+     options = NULL,
+     usage_data = NULL)
---
+++ tests/testthat/test-spec-responses-1.32.R
@@ -0,0 +1,6 @@
+context("spec (responses): 1.32")
+
+## TODO: Missing a response for system_info because 1.32 changes how
+## examples might be reported.  Reconstructing this will be difficult.
+## create_sample_responses("1.32", "1.31")
+test_sample_responses("1.32", "system_df")
