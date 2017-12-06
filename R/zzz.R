eedb8a78e68ed22ca941da98f9c8130465ae2cee
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-12-06 09:28:55 +0000

    Start 1.33 support

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 ## NOTE: When updating versions here, update them throughout the
 ## package - in R/docker_client.R in particular (see the help strings)
 MIN_DOCKER_API_VERSION <- "1.25"
-MAX_DOCKER_API_VERSION <- "1.32"
+MAX_DOCKER_API_VERSION <- "1.33"
 DEFAULT_DOCKER_API_VERSION <- "1.29"
 DEFAULT_DOCKER_UNIX_SOCKET <- "/var/run/docker.sock"
 DEFAULT_USER_AGENT <- "stevedore/0.0.0"
