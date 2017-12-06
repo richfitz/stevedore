829f16b9d047e7a2fead4cf4d72d85092ba10235
Rich FitzJohn <rich.fitzjohn@gmail.com>
2017-12-06 08:12:42 +0000

    Import responses for 1.27

---
+++ tests/testthat/sample_responses/v1.27/system_version.R
@@ -0,0 +1,16 @@
+## version: 1.27
+## method: get
+## path: /version
+## code: 200
+## response: {"Version":"1.13.0","Os":"linux","KernelVersion":"3.19.0-23-generic","GoVersion":"go1.6.3","GitCommit":"deadbee","Arch":"amd64","ApiVersion":"1.25","MinAPIVersion":"1.12","BuildTime":"2016-06-14T07:09:13.444803460+00:00","Experimental":true}
+list(
+  version = "17.04.0",
+  api_version = "1.27",
+  min_apiversion = "1.12",
+  git_commit = "deadbee",
+  go_version = "go1.7.5",
+  os = "linux",
+  arch = "amd64",
+  kernel_version = "3.19.0-23-generic",
+  experimental = TRUE,
+  build_time = "2016-06-14T07:09:13.444803460+00:00")
---
+++ tests/testthat/test-spec-responses-1.27.R
@@ -0,0 +1,4 @@
+context("spec (responses): 1.27")
+
+## create_sample_responses("1.27", "1.28")
+test_sample_responses("1.27", "system_df")
