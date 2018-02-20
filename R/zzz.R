12b30dd446ec9339e017ffd5a39a7b772e88afc5
Rich FitzJohn <rich.fitzjohn@gmail.com>
2018-02-20 07:51:02 +0000

    Revert "Roll back new specs from 7be56d8"
    This reverts commit 907a1573a98bd8641e7103608d7be87f58882582.

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 ## NOTE: When updating versions here, update them throughout the
 ## package - in R/docker_client.R in particular (see the help strings)
 MIN_DOCKER_API_VERSION <- "1.25"
-MAX_DOCKER_API_VERSION <- "1.33"
+MAX_DOCKER_API_VERSION <- "1.36"
 DEFAULT_DOCKER_API_VERSION <- "1.29"
 DEFAULT_DOCKER_UNIX_SOCKET <- "/var/run/docker.sock"
 DEFAULT_DOCKER_WINDOWS_PIPE <- "npipe:////./pipe/docker_engine"
