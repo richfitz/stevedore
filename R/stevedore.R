471cf5023f58114b8d6f9b4871db58e1edfd0aea
Rich FitzJohn <rich.fitzjohn@gmail.com>
2018-02-21 08:42:20 +0000

    Stub package help file

---
+++ R/stevedore.R
@@ -0,0 +1,10 @@
+##' Docker Client For R
+##'
+##' A docker client for R, using \code{curl} and an automatically
+##' generated interface.  To get started, please see the package
+##' vignette (\code{vignette("stevedore")} will work if the package
+##' was installed with the vignettes).
+##'
+##' @name stevedore
+##' @docType package
+NULL
