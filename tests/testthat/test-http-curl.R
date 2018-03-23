42c74c9bc62479d59b13551dec3bc863c489caf1
Rich FitzJohn <rich.fitzjohn@gmail.com>
2018-03-23 08:37:00 +0000

    Curl driver offline testing
    This dates back to 1st of March but was never added

---
+++ tests/testthat/test-http-curl.R
@@ -0,0 +1,21 @@
+context("http with httppipe")
+
+test_that("base_url with https not supported", {
+  expect_error(http_client_curl("http://localhost:8888"),
+               "Providing docker http/https url is not currently supported",
+               fixed = TRUE)
+})
+
+
+test_that("construction", {
+  skip_on_windows()
+
+  cl <- http_client_curl(DEFAULT_DOCKER_UNIX_SOCKET)
+
+  expect_is(cl, "list")
+  expect_equal(cl$type, "curl")
+  expect_is(cl$request, "function")
+  expect_equal(cl$api_version, DEFAULT_DOCKER_API_VERSION)
+  expect_true(cl$can_stream)
+  expect_is(cl$ping, "function")
+})
