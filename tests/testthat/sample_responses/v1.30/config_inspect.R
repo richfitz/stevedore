782eabefafac47fe915042ecae01eb7af00783d9
Rich FitzJohn <rich.fitzjohn@gmail.com>
2018-03-27 09:01:46 +0100

    Add config sample responses

---
+++ tests/testthat/sample_responses/v1.30/config_inspect.R
@@ -0,0 +1,18 @@
+## version: 1.30
+## method: get
+## path: /configs/{id}
+## code: 200
+## response: {"ID":"ktnbjxoalbkvbvedmg1urrz8h","Version":{"Index":11},"CreatedAt":"2016-11-05T01:20:17.327670065Z","UpdatedAt":"2016-11-05T01:20:17.327670065Z","Spec":{"Name":"app-dev.crt","Data":"VEhJUyBJUyBOT1QgQSBSRUFMIENFUlRJRklDQVRFCg=="}}
+list(
+  id = "ktnbjxoalbkvbvedmg1urrz8h",
+  version = list(index = 11L),
+  created_at = "2016-11-05T01:20:17.327670065Z",
+  updated_at = "2016-11-05T01:20:17.327670065Z",
+  spec = list(
+    name = "app-dev.crt",
+    labels = NULL,
+    ## NOTE: in the online api docs:
+    ## https://docs.docker.com/engine/api/v1.30/#operation/ConfigInspect
+    ## the 'data' element is not returned, which is not consistent
+    ## with the spec.
+    data = "VEhJUyBJUyBOT1QgQSBSRUFMIENFUlRJRklDQVRFCg=="))
