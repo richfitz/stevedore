f90a86f7fb58c52fb0e385f1d6f2aea1b07a2e94
Rich FitzJohn <rich.fitzjohn@gmail.com>
2018-04-03 11:21:22 +0100

    Version-specific testing for plugins

--- tests/testthat/test-docker-client-plugins.R
+++ tests/testthat/test-docker-client-plugins.R
@@ -1,7 +1,5 @@
 context("docker client: plugins")
 
-PLUGIN_SSHFS <- "vieux/sshfs:latest"
-
 test_that("privileges", {
   skip_if_no_internet()
   cl <- test_docker_client()
@@ -15,27 +13,38 @@ test_that("privileges", {
 test_that("install", {
   skip_if_no_internet()
 
-  cl <- test_docker_client()
-  key <- rand_str()
-  x <- cl$plugins$install("vieux/sshfs:latest", alias = key, grant_all = TRUE)
-  on.exit(try(x$remove(TRUE)))
+  for (v in c(MIN_DOCKER_API_VERSION,
+              DEFAULT_DOCKER_API_VERSION,
+              MAX_DOCKER_API_VERSION)) {
+    cl <- test_docker_client(api_version = v)
+    key <- rand_str()
+    x <- cl$plugins$install("vieux/sshfs:latest", alias = key,
+                            grant_all = TRUE, stream = FALSE)
+    on.exit(try(x$remove(TRUE)))
 
-  expect_is(x, "docker_plugin")
-  expect_equal(x$name(), paste0(key, ":latest"))
-  expect_is(x$id(), "character")
+    expect_is(x, "docker_plugin")
+    expect_equal(x$name(), paste0(key, ":latest"))
+    expect_is(x$id(), "character")
+    expect_true(x$is_enabled())
 
-  dat <- cl$plugins$list()
-  expect_true(x$id() %in% dat$id)
-  expect_true(x$name() %in% dat$name)
+    dat <- cl$plugins$list()
+    expect_true(x$id() %in% dat$id)
+    expect_true(x$name() %in% dat$name)
 
-  expect_true(dat$enabled[match(x$name(), dat$name)])
-  expect_null(x$disable())
-  expect_false(dat$enabled[match(x$name(), dat$name)])
-  expect_null(x$enable())
-  expect_true(dat$enabled[match(x$name(), dat$name)])
+    expect_true(dat$enabled[match(x$name(), dat$name)])
+    expect_null(x$disable())
+    expect_false(x$is_enabled())
+    dat <- cl$plugins$list()
+    expect_false(dat$enabled[match(x$name(), dat$name)])
 
-  expect_null(x$remove(TRUE))
-  on.exit()
+    expect_null(x$enable())
+    expect_true(x$is_enabled())
+    dat <- cl$plugins$list()
+    expect_true(dat$enabled[match(x$name(), dat$name)])
+
+    expect_null(x$remove(TRUE))
+    on.exit()
+  }
 })
 
 
