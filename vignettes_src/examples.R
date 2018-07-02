ef9864a22513e4431b9b3bfdd7878bdd13b56ed9
Rich FitzJohn <rich.fitzjohn@gmail.com>
2018-07-02 09:13:14 +0100

    Remove DBI/Postgres from vignette

--- vignettes_src/examples.R
+++ vignettes_src/examples.R
@@ -17,23 +17,6 @@ lang_output <- function(x, lang) {
 c_output <- function(x) lang_output(x, "cc")
 r_output <- function(x) lang_output(x, "r")
 plain_output <- function(x) lang_output(x, "plain")
-pg_ready <- function(port, host = "localhost", user = "postgres",
-                     attempts = 10) {
-  f <- function() {
-    con <- DBI::dbConnect(RPostgres::Postgres(),
-                          host = host, port = port, user = user)
-    on.exit(DBI::dbDisconnect(con))
-    DBI::dbListTables(con)
-    TRUE
-  }
-  for (i in seq_len(attempts)) {
-    if (tryCatch(f(), error = function(e) FALSE)) {
-      return()
-    }
-    Sys.sleep(1)
-  }
-  stop("postgres not available in time")
-}
 
 ## ## Using a database in testing
 
