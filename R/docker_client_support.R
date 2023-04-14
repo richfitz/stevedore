38034eb9e4d1a23a06f6e7353dbcdf86edcf09ac
Teofil <teofiln@gmail.com>
2023-04-13 21:15:54 -0600

    testing if pretty_bytes causes the problem on image push

--- R/docker_client_support.R
+++ R/docker_client_support.R
@@ -118,7 +118,8 @@ pull_status_printer <- function(stream = stdout()) {
       cur <- x$progressDetail[["current"]]
       tot <- x$progressDetail[["total"]]
       str <- sprintf("%s: %s %s/%s %d%%%s", x[["id"]], x[["status"]],
-                     pretty_bytes(cur), pretty_bytes(tot),
+                     # pretty_bytes(cur), pretty_bytes(tot),
+                     cur, tot,
                      round(cur / tot * 100),
                      endl)
     } else {
